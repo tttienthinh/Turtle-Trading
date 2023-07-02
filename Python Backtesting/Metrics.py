93e7660b5f465cd247fdc4cfdb2197538b71c78f
JessicaRanto <121685144+JessicaRanto@users.noreply.github.com>
2023-07-02 20:59:10 +0200

    Update Metrics.py

--- Python Backtesting/Metrics.py
+++ Python Backtesting/Metrics.py
@@ -8,7 +8,9 @@ class Strategy:
         self.name = name
         self.returns = returns
         self.pnl = np.c_[np.array([100.]), 100.*np.cumprod(1.+returns.to_numpy()).reshape(1,-1)].flatten()
-
+        
+#Annual volatility/ 252 is the trading days in a year
+    
     def volatility(self):
         return np.std(self.returns)*np.sqrt(252)
 
@@ -24,7 +26,7 @@ class Strategy:
     def illustrate(self):
         fig, ax = plt.subplots(1, 1)
         ax.plot(self.returns.index, self.pnl[1:], 'b', label=self.name + ' (vol: %.2f, Sharpe: %.2f, MDD: %.2f)'%
-                                                          (self.volatility(), self.sharpe_ratio(), self.max_dd()))
+                                                          (self.volatility(), self.sharpe_ratio(), self.max_drawdown()))
         ax.set_xlabel('Time')
         ax.set_ylabel('PnL')
         ax.legend()
@@ -35,14 +37,3 @@ class Strategy:
         fig.savefig(f'{self.name}.pdf')
         plt.show()
 
-
-class CapiWeighted(Strategy):
-
-    def __init__(self, name, returns, capitalizations):
-        weights_CW = capitalizations.copy()
-        weights_CW['sum'] = weights_CW.sum(axis=1)
-        weights_CW = weights_CW.div(weights_CW['sum'], axis=0)
-        weights_CW.drop(columns='sum', inplace=True)
-        weights_CW = weights_CW.shift(1).dropna()
-        returns_CW = (returns * weights_CW).sum(axis=1)
-        super().__init__(name, returns_CW)
